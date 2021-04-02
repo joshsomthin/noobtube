cef554ee9f05233d0c513850c77aabe286f8863c
joshua <joshuagmiller2289@gmail.com>
2021-04-02 16:00:08 -0700

    database structure complete thunks created for carousel components

--- app/seeds/game.py
+++ app/seeds/game.py
@@ -2,13 +2,13 @@ import requests
 import os
 import json
 from datetime import datetime, date
-from app.models import db, Game, GameTag, Tag
+from app.models import db, Game, Tag
 
 
 def seed_games(url="https://rawg-video-games-database.p.rapidapi.com/games"):
 
     allgenres = Tag.query.all()
-    genres = {genre.name: genre.id for genre in allgenres}
+    genres = {genre.name: genre for genre in allgenres}
 
     headers = {
         'x-rapidapi-key': os.environ.get('RAPIDAPI_KEY'),
@@ -26,9 +26,7 @@ def seed_games(url="https://rawg-video-games-database.p.rapidapi.com/games"):
             db.session.add(game)
             db.session.commit()
             for genre in res['genres']:
-                genreId = genres[genre['name']]
-                genre = GameTag(game_id=game.id, tag_id=genreId)
-                db.session.add(genre)
-            db.session.commit()
+                genre_obj = genres[genre['name']]
+                game.tags.append(genre_obj)
     if json_data['next']:
         seed_games(json_data['next'])
