e254042c5c3d6a661b65306b4eb86bb7505a6b29
joshua <joshuagmiller2289@gmail.com>
2021-05-08 13:10:39 -0700

    working on comment form

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -1,4 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Comment
+
+
+class CommentForm(FlaskForm):
+    video_id =
