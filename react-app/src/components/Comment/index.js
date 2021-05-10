795fd45fa50b79bac8a8c002163325b35f8f347e
joshua <joshuagmiller2289@gmail.com>
2021-05-10 16:44:37 -0700

    button spacing for edit/delete buttons fixed

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
 import "./Comment.css";
+import { ButtonGroup } from "@material-ui/core";
+
+const editDeleteButtons = (
+  <ButtonGroup>
+    <button>Edit</button>
+    <button>Delete</button>
+  </ButtonGroup>
+);
 
 const Comment = ({ body, user, user_id, date }) => {
+  const loggedInUser = useSelector((state) => state?.user?.user?.id);
   return (
     <div className="comment">
       <div className="account-icon">
@@ -11,7 +21,7 @@ const Comment = ({ body, user, user_id, date }) => {
           <AccountCircleRoundedIcon />
         </NavLink>
       </div>
-      <div>
+      <div className="text">
         <div className="username">
           <NavLink to={`/user/${user_id}`}>
             <span>{user}</span>
@@ -21,7 +31,10 @@ const Comment = ({ body, user, user_id, date }) => {
             {date !== 0 ? date : "today"} {date === 0 ? "" : "days ago"}
           </span>
         </div>
-        <span>{body}</span>
+        <div className="body">
+          <span>{body}</span>
+          {loggedInUser === user_id ? editDeleteButtons : ""}
+        </div>
       </div>
     </div>
   );
